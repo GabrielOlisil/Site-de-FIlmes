aba05d8fb9537a852a5d50057a0fe7003ce1a65c
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2023-01-08 16:09:29 -0400

    inserindo mais informações na chamada da api

--- api.ts
+++ api.ts
@@ -1,20 +1,29 @@
 import axios from "axios";
-import { fetchDiscover } from "./interfaces/filme";
+import { DiscoverSearch } from "./interfaces/filme";
 
 const httpClient = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params: {
         api_key: process.env.NEXT_PUBLIC_API_KEY,
         language: "pt-BR",
-        include_video: true
+        watch_region: "pt-BR",
+        sort_by: "popularity.desc",
+        locale: "BR"
     }
 })
 
 export const API = {
     loadData: async (page: number) => {
         httpClient.defaults.params.page = page;
-        let filmes = await httpClient.get("/discover/movie");
+        let requisicao = await httpClient.get("/discover/movie");
 
-        return filmes.data.results;
+        let filmes: DiscoverSearch = requisicao.data
+
+        filmes.results.forEach(async (filme) =>{
+            let requisicao = await httpClient.get(`/movie/${filme.id}?`)
+            filme.informations = requisicao.data
+        });
+
+        return filmes;
     }
 }
---
+++ interfaces/filme.ts
@@ -0,0 +1,55 @@
+
+export interface Filme {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string; 
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    informations: FilmeInformations;
+}
+
+
+export interface DiscoverSearch {
+    results: Filme[];
+    page: number;
+    total_pages: number;
+    total_results: number;
+}
+
+
+export interface FilmeInformations {
+    adult: boolean;
+    backdrop_path: string | null;
+    belongs_to_collection: {backdrop_path: null | string, id: number, name: string, poster_path: null | string} | null
+    budget: number;
+    genre: Array<{id: number, name: string}>;
+    homepage: string | null;
+    id: number;
+    imdb_id: string | null;
+    original_language: string;
+    original_title: string;
+    overview: string | null;
+    popularity: number;
+    poster_path: string | null;
+    production_companies: Array<{name: string, id: number, logo_path: string | null, origin_country: string}>
+    production_countries: Array<{iso_3166_1: string, name: string}>
+    release_date: string;
+    revenue: number;
+    runtime: number | null;
+    spoken_languages: Array<{iso_639_1: string, name: string}>;
+    status: "Rumored" |  "Planned" | "In Production"| "Post Production"| "Released"| "Canceled";
+    tagline: string | null;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
